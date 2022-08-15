d0598430e11ac544a4422d217eb3223d2d84857d
Titeuf67 <christian.fillette@gmail.com>
2022-08-16 00:03:44 +0200

    15/08

--- api/router.js
+++ api/router.js
@@ -1,7 +1,6 @@
 const fkdb = require("./config/db.json")
 const express = require("express");
 const router = express.Router()
-
 const db = require('./config/db')
 
 // db.query('select * from permis', (err, data) => {
@@ -44,11 +43,11 @@ router
     })
     .post('/permis', (req, res) => {
         console.log('POST::permis', req.body)
-        const { name, description } = req.body;
+        const { name, description, image } = req.body;
 
         db.query(`
-    INSERT INTO permis (name, description)
-        VALUES ("${name}", "${description}")
+    INSERT INTO permis (name, description, image)
+        VALUES ("${name}", "${description}", "${image}")
     `, (err, data) => {
             db.query(`SELECT * from permis WHERE id = ${data.insertId}`, (err, obj) => {
                 db.query('SELECT * FROM permis', (err, arr) => {
@@ -71,11 +70,12 @@ router
     // })
 
     .put('/permis/:id', async (req, res) => {
-        const { name, description } = req.body;
+        const { name, description, image } = req.body;
         const { id } = req.params;
 
         if (name) await db.query(`UPDATE permis set name = "${name}" WHERE id = ${id};`)
         if (description) await db.query(`UPDATE permis set description = "${description}" WHERE id = ${id};`)
+        if (image) await db.query(`UPDATE permis set image = "${image}" WHERE id = ${id};`)
 
         res.redirect('/admin')
     })
@@ -96,12 +96,57 @@ router.get('/permis/:id',async (req, res) =>{
 // fin des router table permis
 
 // router crud table users
+router.post('/user', (req, res) => {
+    console.log('POST::user', req.body)
+    const { nom, prenom, telephone, email, identifiant, password } = req.body;
 
+    
 
+    db.query(`
+INSERT INTO user (nom, prenom, telephone, email, identifiant, password)
+    VALUES ("${nom}", "${prenom}", "${telephone}", "${email}", "${identifiant}", "${password}")
+`, (err, data) => {
+        db.query(`SELECT * from user WHERE id = ${data.insertId}`, (err, obj) => {
+            db.query('SELECT * FROM user', (err, arr) => {
+                // res.json({ ...req.body, data, obj, arr })
+                res.redirect('/admin')
+            })
+        })
 
+    })
+})
 
+router.delete('/user/:id', async (req, res) => {
+    const { id } = req.params;
 
+    await db.query(`DELETE from user WHERE id = ${id}`)
+    res.redirect('/admin')
+})
 
+router.post('/forminscription', (req, res) => {
+    
+    const { nom, prenom, telephone, email, identifiant, password, mdp } = req.body;
+
+    if(password != mdp){
+        console.error('ERROR PASSWORD NOT EQUALS')
+        return
+    }
+
+    console.log('POST::user', req.body)
+
+    db.query(`
+INSERT INTO user (nom, prenom, telephone, email, identifiant, password)
+    VALUES ("${nom}", "${prenom}", "${telephone}", "${email}", "${identifiant}", "${password}")
+`, (err, data) => {
+        db.query(`SELECT * from user WHERE id = ${data.insertId}`, (err, obj) => {
+            db.query('SELECT * FROM user', (err, arr) => {
+                // res.json({ ...req.body, data, obj, arr })
+                res.redirect('/admin')
+            })
+        })
+
+    })
+})
 
 
 
---
+++ api/user.sql
@@ -0,0 +1,1 @@
+create table user (id int(50) not null auto_increment primary key, nom varchar(255), prenom varchar(255), telephone varchar(255), email varchar(255), identifiant varchar(255), password varchar(255));
